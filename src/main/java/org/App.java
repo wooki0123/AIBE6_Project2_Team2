6c4be8cc0609f6793be24147e616cadf96b43a64
wooki0123 <wookimini22@gmail.com>
2026-04-13 17:21:45 +0900

    feat: 명령어 입력 및 게시물 기본생성자 구현

--- src/main/java/org/App.java
+++ src/main/java/org/App.java
@@ -1,4 +1,31 @@
 package org;
 
+import java.util.Scanner;
+
 public class App {
+    void run() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("프로그램 실행");
+        System.out.print("명령어: ");
+        String cmd = scanner.nextLine().trim();
+        System.out.println("입력된 명령어: " + cmd);
+
+        while (true) {
+            switch (cmd) {
+                case "write":
+                    break;
+                case "list":
+                    break;
+                case "detail":
+                    break;
+                case "update":
+                    break;
+                case "delete":
+                    break;
+                default:
+                    System.out.println("알 수 없는 명령어입니다.");
+            }
+        }
+    }
+
 }
--- src/main/java/org/Article.java
+++ src/main/java/org/Article.java
@@ -1,4 +1,17 @@
 package org;
 
+import java.time.LocalDateTime;
+
 public class Article {
+    private int id;
+    private String title;
+    private String content;
+    private LocalDateTime createDate;
+
+    Article(String title, String content) {
+        LocalDateTime now = LocalDateTime.now();
+        this.title = title;
+        this.content = content;
+        this.createDate = now;
+    }
 }
