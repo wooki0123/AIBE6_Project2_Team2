b214cb97edf8d05617fc19acbfb325d717d068ed
wooki0123 <wookimini22@gmail.com>
2026-04-14 16:44:31 +0900

    feat: ArticleController 구í-2

--- src/main/java/org/App.java
+++ src/main/java/org/App.java
@@ -1,5 +1,6 @@
 package org;
 
+import org.domain.article.ArticleController;
 import org.domain.system.SystemController;
 
 import java.util.ArrayList;
@@ -12,6 +13,7 @@ public class App {
     private List<Article> articleList = new ArrayList<>();
     Scanner scanner = new Scanner(System.in);
     SystemController systemController = new SystemController();
+    ArticleController articleController = new ArticleController(scanner);
 
     void run() {
         System.out.println("프로그램 실행");
@@ -23,11 +25,11 @@ public class App {
             String actionName = cmdBits[0];
 
             switch (actionName) {
-                case "write" -> actionWrite();
-                case "list" -> actionList();
-                case "detail" -> actionDetail(cmd);
-                case "update" -> actionUpdate(cmd);
-                case "delete" -> actionDelete(cmd);
+                case "write" -> articleController.actionWrite();
+                case "list" -> articleController.actionList();
+                case "detail" -> articleController.actionDetail(cmd);
+                case "update" -> articleController.actionUpdate(cmd);
+                case "delete" -> articleController.actionDelete(cmd);
                 case "exit" -> {
                     systemController.actionExit();
                     return;
@@ -35,60 +37,5 @@ public class App {
 
             }
         }
-    }
-
-    void actionWrite() {
-        System.out.print("제목: ");
-        String title = scanner.nextLine().trim();
-        System.out.print("내용: ");
-        String content = scanner.nextLine().trim();
-
-        ++lastId;
-
-        Article article = new Article(lastId, title, content);
-
-        articleList.add(article);
-
-        System.out.println("=> 게시글이 등록되었습니다.");
-    }
-
-    void actionList() {
-        System.out.println("번호 | 제목       | 등록일");
-        System.out.println("-----------------------------");
-        for (int i = articleList.size() - 1; i >= 0; i--) {
-            Article article = articleList.get(i);
-            System.out.println("%d | %s | %s".formatted(article.getId(), article.getTitle(), article.getCreateDate()));
-        }
-    }
-
-    void actionDetail(String cmd) {
-        String[] cmdBits = cmd.split(" ");
-        if (cmdBits.length < 2) {
-            System.out.println("명령어를 확인해주세요.");
-            return;
-        }
-
-        int articleId = Integer.parseInt(cmdBits[1]);
-        for (Article article : articleList) {
-            if (article.getId() == articleId) {
-                System.out.println("번호: %d".formatted(article.getId()));
-                System.out.println("제목: %s".formatted(article.getTitle()));
-                System.out.println("내용: %s".formatted(article.getContent()));
-                System.out.println("등록일: %s".formatted(article.getCreateDate()));
-                return;
-            } else {
-                System.out.println("해당 번호의 게시글이 존재하지 않습니다.");
-                return;
-            }
-        }
-
-    }
-
-    void actionUpdate(String cmd) {
-
-    }
-
-    void actionDelete(String cmd) {
-
     }
 }
--- src/main/java/org/domain/article/ArticleController.java
+++ src/main/java/org/domain/article/ArticleController.java
@@ -1,4 +1,72 @@
 package org.domain.article;
 
+import org.Article;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class ArticleController {
+    private final Scanner scanner;
+    private int lastId = 0;
+    private List<Article> articleList = new ArrayList<>();;
+
+    public ArticleController(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    public void actionWrite() {
+        System.out.print("제목: ");
+        String title = scanner.nextLine().trim();
+        System.out.print("내용: ");
+        String content = scanner.nextLine().trim();
+
+        ++lastId;
+
+        Article article = new Article(lastId, title, content);
+
+        articleList.add(article);
+
+        System.out.println("=> 게시글이 등록되었습니다.");
+    }
+
+    public void actionList() {
+        System.out.println("번호 | 제목       | 등록일");
+        System.out.println("-----------------------------");
+        for (int i = articleList.size() - 1; i >= 0; i--) {
+            Article article = articleList.get(i);
+            System.out.println("%d | %s | %s".formatted(article.getId(), article.getTitle(), article.getCreateDate()));
+        }
+    }
+
+    public void actionDetail(String cmd) {
+        String[] cmdBits = cmd.split(" ");
+        if (cmdBits.length < 2) {
+            System.out.println("명령어를 확인해주세요.");
+            return;
+        }
+
+        int articleId = Integer.parseInt(cmdBits[1]);
+        for (Article article : articleList) {
+            if (article.getId() == articleId) {
+                System.out.println("번호: %d".formatted(article.getId()));
+                System.out.println("제목: %s".formatted(article.getTitle()));
+                System.out.println("내용: %s".formatted(article.getContent()));
+                System.out.println("등록일: %s".formatted(article.getCreateDate()));
+                return;
+            } else {
+                System.out.println("해당 번호의 게시글이 존재하지 않습니다.");
+                return;
+            }
+        }
+
+    }
+
+    public void actionUpdate(String cmd) {
+
+    }
+
+    public void actionDelete(String cmd) {
+
+    }
 }
